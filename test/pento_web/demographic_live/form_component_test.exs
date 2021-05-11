3b284e7699fd421233f0165ae8622351f13e40e1
Michael Johnston <lastobelus@mac.com>
2021-05-11 14:23:33 -0700

    feature: Survey WIP -- add form_component for the demographic section

---
+++ test/pento_web/demographic_live/form_component_test.exs
@@ -0,0 +1,13 @@
+defmodule PentoWeb.DemographicLive.FormComponentTest do
+  @moduledoc false
+  
+  use ExUnit.Case
+
+  alias PentoWeb.DemographicLive.FormComponent
+
+  describe "PentoWeb.DemographicLive.FormComponent" do
+    test "write some tests" do
+      flunk("no tests for #{PentoWeb.DemographicLive.FormComponent} yet!")
+    end
+  end
+end
